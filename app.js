1b1d92de126c479abf8c4da6a5fe3d6a06b31f70
Abhishek Singla <singla.abhishek97@gmail.com>
2020-10-03 13:05:33 +0530

    add create comment feature

--- app.js
+++ app.js
@@ -70,13 +70,46 @@ app.post("/campgrounds", function(req, res){
 
 //SHOW Route
 app.get("/campgrounds/:id", function(req, res){
-    var query = 'SELECT * FROM Campground LEFT JOIN Comment ON Campground.id = Comment.camp_id WHERE id = ' + req.params.id
+    var query = 'SELECT * FROM Campground LEFT JOIN Comment ON Campground.id = Comment.camp_id WHERE id = ' + req.params.id;
     connection.query(query, function(err, foundcamprows){
         if(err){
             console.log(err);
         } else{
             res.render("campgrounds/show", {camp:foundcamprows});
         }
+    })
+})
+
+//=========================
+//    COMMENT ROUTES
+//=========================
+
+//New comment route
+app.get("/campgrounds/:id/comments/new", function(req, res){
+    console.log(req.params.id);
+    // res.send("you have ht new comment page");
+    var query = 'SELECT * FROM Campground WHERE id = ' + req.params.id;
+    connection.query(query, function(err, foundcamp){
+        if(err){
+            console.log(err);
+        } else{
+            res.render("comments/new", {camp: foundcamp[0]});
+        }
+    })
+})
+
+//Create Comment Route
+app.post("/campgrounds/:id/comments", function(req, res){
+    // res.send("You hit the post route");
+    var newComment  = req.body.comment;
+    newComment.camp_id = req.params.id;
+    connection.query('INSERT INTO Comment SET ?', newComment, function(err){
+        if(err) {
+            console.log(err);
+        } else{
+            console.log("comment added successfully");
+        }
+        res.redirect("/campgrounds/" + req.params.id);
     })
 })
 
